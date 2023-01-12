c0ea30b3d356a87e236583a6955a1b18dc9c7475
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-11 19:59:55 -0800

    fix seed data

--- Develop/seeds/product-tag-seeds.js
+++ Develop/seeds/product-tag-seeds.js
@@ -9,18 +9,20 @@ const productTagData = [
     product_id: 1,
     tag_id: 7,
   },
-  {
-    product_id: 1,
-    tag_id: 8,
-  },
+  //I believe this is a mistake in the seeds! Product_id: 1 is "plain T-shirts", and tag_id: 8 is pop culture. This doesn't match. 
+  // {
+  //   product_id: 1,
+  //   tag_id: 8,
+  // },
   {
     product_id: 2,
     tag_id: 6,
   },
-  {
-    product_id: 3,
-    tag_id: 1,
-  },
+  //I believe this is also a mistake in the seeds! product_id: 3 is "Branded Baseball Hat", and tag_id: 1 is "Rock Music". This doesn't match. 
+  // {
+  //   product_id: 3,
+  //   tag_id: 1,
+  // },
   {
     product_id: 3,
     tag_id: 3,
